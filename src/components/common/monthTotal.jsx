97383024d9790ef1c7d1ea90a83b933f4d16b5cb
Shoaib Masood <ms.masood037@gmail.com>
2021-09-20 09:16:39 +0500

    Completed add expense functionality

--- src/components/common/monthTotal.jsx
+++ src/components/common/monthTotal.jsx
@@ -1,16 +1,29 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 const MonthTotal = (props) => {
   const records = useSelector((state) => state.records);
-  const pageData = records?.data;
+  const pageData = records?.records;
+  let [monthTotal, setMonthTotal] = useState(0);
 
   const date = new Date();
   const currentMonth = date.toLocaleString('default', { month: 'short' });
 
-  useEffect(() => {
+  const sumTotal = (arr) => {
+    let sum = 0;
+    for (const input of arr) {
+      if (!!input.amount) sum += Number(input.amount);
+    }
+    return sum;
+  }
 
-  });
+  useEffect(() => {
+    if(!!pageData) {
+      let temp = sumTotal(pageData);
+      if(!!temp)
+        setMonthTotal(temp);
+    }
+  }, [pageData]);
 
   return (
     <div className="form-row">
@@ -18,7 +31,7 @@ const MonthTotal = (props) => {
         <label>{currentMonth}:</label>
       </div>
       <div className="form-fields">
-        <span className="form-fieldss">${"123"}</span>
+        <span className="form-fieldss">${monthTotal}</span>
       </div>
     </div>
   );
